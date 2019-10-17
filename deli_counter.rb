51e25a42d5f76dca12c02074b8af14530ef83cff
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-17 02:06:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(line)
   if line.length == 0
     puts "The line is currently empty."
   else
-    line_length = "The line is currently: "
+    line_length = "The line is currently:"
     line.each.with_index(1) do |person, x|
-      line_length << "#{x}. #{person} "
+      line_length << " #{x}. #{person}"
     end
     puts line_length
   end
