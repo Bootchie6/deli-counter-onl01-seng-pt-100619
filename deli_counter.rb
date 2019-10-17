a43159185ce0b186d866f55478d0220e254e7ec2
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-17 02:00:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(line)
   if line.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is #{line.length} person(s) long."
+    line_length = "The line is #{line.length} person(s) long."
   end
-  
+  puts line_length
 end
 
 def take_a_number
