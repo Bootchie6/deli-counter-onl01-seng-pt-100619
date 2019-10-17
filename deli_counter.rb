368f66d36ba311d3fe72ffaca44a1da06092599d
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-17 02:16:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(line)
   if line.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Serving #{line.first}."
+    puts "Currently serving #{line.first}."
     line.shift
   end
 end
