116879bbb17079a43e23fe427ff9eb8b729e370b
Jonathan Jopio <jonathanjopio@gmail.com>
2019-10-17 01:57:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def line(line)
+  if line.length == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is #{line.length} person(s)"
+  end
+end
+
+def take_a_number
+  
+end
+
+def now_serving
+  
+end
